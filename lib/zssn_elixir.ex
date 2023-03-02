54d31bb78bb7b39bf2ebaf80e268c18d27bb8f8b
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2023-03-01 19:00:18 -0600

    Build a new Elixir project

---
+++ lib/zssn_elixir.ex
@@ -0,0 +1,18 @@
+defmodule ZssnElixir do
+  @moduledoc """
+  Documentation for `ZssnElixir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ZssnElixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/zssn_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule ZssnElixirTest do
+  use ExUnit.Case
+  doctest ZssnElixir
+
+  test "greets the world" do
+    assert ZssnElixir.hello() == :world
+  end
+end
